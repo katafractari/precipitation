3f36f2f6036feecf5fa66c2f18ec975d4de07929
Rok Pergarec <rok@parabola.si>
2016-05-17 23:34:45 +0200

    introduce scheduler, create precipitation module

--- app.js
+++ app.js
@@ -3,9 +3,19 @@
 var config = require('config');
 var util = require('util');
 var express = require('express');
+var schedule = require('node-schedule');
+var precipitation = require('./src/js/modules/precipitation');
+// TODO: make Datastore object a singleton / figure out how to inject Datastore as dependency
 var Datastore = require('nedb')
     , db = new Datastore({ filename: config.get('db.filename'), autoload: true });
 
+// Update precipitation task
+// TODO: Test scheduled job
+schedule.scheduleJob('0 * * * *', function(){
+    precipitation.update();
+});
+
+// Web server
 var app = express();
 app.use(express.static('.'));
 
